b482c8d4906f8aeaab1c43a61bb872350e6a24e3
ivanasen <ivanasenchakarov@gmail.com>
2020-08-16 16:36:30 +0300

    Add comments for yolo loss function

--- models/yolo/model.py
+++ models/yolo/model.py
@@ -144,7 +144,7 @@ def _boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
     boxes = tf.reshape(boxes, [-1, 4])
     box_scores = box_confidence * box_class_probs
     box_scores = tf.reshape(box_scores, [-1, num_classes])
-    
+
     return boxes, box_scores
 
 
@@ -235,6 +235,7 @@ def compute_loss(pred, conv, label, boxes, i=0):
     batch_size = conv_shape[0]
     output_size = conv_shape[1]
     input_size = config.STRIDES[i] * output_size
+    input_size = tf.cast(input_size, tf.float32)
     conv = tf.reshape(conv, (batch_size, output_size,
                              output_size, 3, 5 + 1))
 
@@ -248,23 +249,20 @@ def compute_loss(pred, conv, label, boxes, i=0):
     respond_box = label[:, :, :, :, 4:5]
     label_prob = label[:, :, :, :, 5:]
 
+    # Localization loss (Using GIoU)
     giou = tf.expand_dims(utils.box_giou(pred_xywh, label_xywh), axis=-1)
-    input_size = tf.cast(input_size, tf.float32)
-
     box_loss_scale = 2.0 - 1.0 * \
         label_xywh[:, :, :, :, 2:3] * \
         label_xywh[:, :, :, :, 3:4] / (input_size ** 2)
     giou_loss = respond_box * box_loss_scale * (1 - giou)
 
+    # Confidence loss
     iou = utils.box_iou(pred_xywh[:, :, :, :, np.newaxis, :],
                         boxes[:, np.newaxis, np.newaxis, np.newaxis, :, :])
     max_iou = tf.expand_dims(tf.reduce_max(iou, axis=-1), axis=-1)
-
     respond_bgd = (1.0 - respond_box) * \
         tf.cast(max_iou < config.IOU_LOSS_THRESHOLD, tf.float32)
-
     conf_focal = tf.pow(respond_box - pred_conf, 2)
-
     conf_loss = conf_focal * (
         respond_box *
         tf.nn.sigmoid_cross_entropy_with_logits(
@@ -272,15 +270,15 @@ def compute_loss(pred, conv, label, boxes, i=0):
         +
         respond_bgd *
         tf.nn.sigmoid_cross_entropy_with_logits(
-            labels=respond_box, logits=conv_raw_conf)
-    )
+            labels=respond_box, logits=conv_raw_conf))
 
-    prob_loss = respond_box * \
+    # Classification loss
+    class_loss = respond_box * \
         tf.nn.sigmoid_cross_entropy_with_logits(
             labels=label_prob, logits=conv_raw_prob)
 
     giou_loss = tf.reduce_mean(tf.reduce_sum(giou_loss, axis=[1, 2, 3, 4]))
     conf_loss = tf.reduce_mean(tf.reduce_sum(conf_loss, axis=[1, 2, 3, 4]))
-    prob_loss = tf.reduce_mean(tf.reduce_sum(prob_loss, axis=[1, 2, 3, 4]))
+    class_loss = tf.reduce_mean(tf.reduce_sum(class_loss, axis=[1, 2, 3, 4]))
 
-    return giou_loss, conf_loss, prob_loss
+    return giou_loss, conf_loss, class_loss
